f3204da455bf0799b807d6a7c87672fba2b3ed62
Papa Daco <papadaco@PapaDaco.local>
2023-05-18 10:30:27 -0400

    Init setup w/ Firebase connection

--- android/app/src/main/kotlin/com/example/once_upon_a_time/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/once_upon_a_time/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.once_upon_a_time
+package com.dtfun.once_upon_a_time
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:path_provider/path_provider.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'package:once_upon_a_time/firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // FirebaseMessaging.onBackgroundMessage(backgroundHandler);
+
+  // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  // Bloc.observer = SimpleBlocObserver();
+  runApp(const OnceUponATime());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class OnceUponATime extends StatelessWidget {
+  const OnceUponATime({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,25 +31,70 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Once Upon a Time'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: const Center(
-        child: Text('Coming Soon to an App Near You'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Once Upon A Time'),
+        ),
+        body: const Center(
+          child: Text('Derp'),
+        ),
       ),
     );
+    // return MultiRepositoryProvider(
+    //   providers: [
+    //     RepositoryProvider(
+    //       create: (context) => UserRepository(),
+    //     ),
+    //     RepositoryProvider(
+    //       create: (context) => AuthRepository(
+    //         userRepository: context.read<UserRepository>(),
+    //       ),
+    //     ),
+    //   ],
+    //   child: MultiBlocProvider(
+    //     providers: [
+    //       BlocProvider(
+    //         create: (context) => AuthBloc(
+    //           authRepository: context.read<AuthRepository>(),
+    //           userRepository: context.read<UserRepository>(),
+    //         ),
+    //       ),
+    //       BlocProvider(
+    //         create: (context) => UserBloc(
+    //           authBloc: context.read<AuthBloc>(),
+    //           userRepository: context.read<UserRepository>(),
+    //           //   )..add(
+    //           //       LoadUser(
+    //           //         context.read<AuthBloc>().state.authUser,
+    //           //       ),
+    //         ),
+    //       ),
+    //       BlocProvider(
+    //         create: (context) => BrightnessCubit(),
+    //       ),
+    //       BlocProvider(
+    //         create: (context) => LoginCubit(
+    //           authRepository: context.read<AuthRepository>(),
+    //           userRepository: context.read<UserRepository>(),
+    //         ),
+    //       ),
+    //       BlocProvider(
+    //         create: (context) => SignUpCubit(
+    //           authRepository: context.read<AuthRepository>(),
+    //         ),
+    //       ),
+    //     ],
+    //     child: BlocBuilder<BrightnessCubit, Brightness>(
+    //       builder: (context, state) {
+    //         return MaterialApp(
+    //           debugShowCheckedModeBanner: false,
+    //           theme: state == Brightness.dark ? darkTheme() : lightTheme(),
+    //           initialRoute: SplashScreen.routeName,
+    //           onGenerateRoute: AppRouter.onGenerateRoute,
+    //         );
+    //       },
+    //     ),
+    //   ),
+    // );
   }
 }
